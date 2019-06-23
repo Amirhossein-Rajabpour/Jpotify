a7ea6e60b3eaa495cf78812c63e2e70c15661675
Amirhosein Rajabpour <9731085@ceit.local>
2019-06-23 10:20:35 -0700

    checkbox and button actions completed

--- src/view/NewPlaylist.java
+++ src/view/NewPlaylist.java
@@ -3,13 +3,22 @@ package view;
 import model.Song;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class NewPlaylist extends JFrame {
+public class NewPlaylist extends JFrame implements ActionListener {
 
-    JTextField name;
-    JLabel nameLabel;
-    JButton create;
+    private JTextField name;
+    private JLabel nameLabel;
+    private JButton create;
+    private JCheckBox[] songCheckbox;
+    private int size;
+    private String playlistName;
+    private ArrayList<Song> songs;
+    HashMap<String,ArrayList<Song>> Playlist = new HashMap<>(); // this HashMap is for Playlist
+
 
     public NewPlaylist(ArrayList<Song> songs){
 
@@ -17,14 +26,36 @@ public class NewPlaylist extends JFrame {
         this.setLayout(new FlowLayout());
 
         nameLabel = new JLabel("what is the name of new playlist");
-        name = new JTextField();
+        add(nameLabel);
 
-        int size = songs.size();
-        JCheckBox[] songCheckbox = new JCheckBox[size];
+
+        name = new JTextField();
+        playlistName = name.getText();
+        add(name);
+
+        size = songs.size();
+        songCheckbox = new JCheckBox[size];
         for(int i = 0; i < size ; i++){
 
             songCheckbox[i] = new JCheckBox(songs.get(i).getTitle());
+            add(songCheckbox[i]);
+//            songCheckbox[i].addActionListener(this);
+        }
+
+        create = new JButton("create");
+        create.addActionListener(this);
+        add(create);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        for(int i = 0; i < size; i++){
+
+            if(songCheckbox[i].isSelected()){
+                Playlist.get(playlistName).add(songs.get(i));
+            }
+
         }
     }
-    
 }
