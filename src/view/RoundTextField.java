a46c0b30ede4a8a3602165853c157ff5fbb260ee
9731087 <9731087@ceit.local>
2019-06-24 00:49:34 +0430

    SearchBox feature improved

--- src/view/RoundTextField.java
+++ src/view/RoundTextField.java
@@ -28,20 +28,20 @@ public class RoundTextField extends JTextField {
     @Override
     protected void paintComponent(Graphics g) {
         g.setColor(Color.white);
-        g.fillRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 25, 25);
+        g.fillRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 15, 15);
         super.paintComponent(g);
     }
 
     @Override
     protected void paintBorder(Graphics g) {
         g.setColor(backgroundColor);
-        g.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 25, 25);
+        g.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 15, 15);
     }
 
     @Override
     public boolean contains(int x, int y) {
         if (shape == null || !shape.getBounds().equals(getBounds())) {
-            shape = new RoundRectangle2D.Float(0, 0, getWidth() - 1, getHeight() - 1, 25, 25);
+            shape = new RoundRectangle2D.Float(0, 0, getWidth() - 1, getHeight() - 1, 15, 15);
         }
         return shape.contains(x, y);
     }
