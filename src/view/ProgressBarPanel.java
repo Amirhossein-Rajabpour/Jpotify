5e7f05df9f1f402714ac81b0a4489c0382fa37d5
9731087 <9731087@ceit.local>
2019-06-24 20:19:59 +0430

    ProgressBarPanel class created and the progressBar passed to Player part through BottomPanel

---
+++ src/view/ProgressBarPanel.java
@@ -0,0 +1,60 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ProgressBarPanel extends JPanel {
+
+    private Color background;
+    private JProgressBar progressBar;
+    private int num;
+
+
+    public ProgressBarPanel() {
+
+        background = new Color(40, 40, 40);
+        this.setForeground(new Color(179, 179, 179));
+        this.setBackground(background);
+
+        JTextField passed = new JTextField("00:00");
+        passed.setFont(new Font("Arial", Font.CENTER_BASELINE, 9));
+        passed.setEditable(false);
+        passed.setBackground(background);
+        passed.setForeground(new Color(179, 179, 179));
+        this.add(passed);
+
+
+//        UIManager.put("ProgressBar.selectionBackground",Color.WHITE);
+//        UIManager.put("ProgressBar.selectionForeground",Color.WHITE);
+        progressBar = new JProgressBar(0, 1000);
+        progressBar.setStringPainted(false);
+        progressBar.setBorderPainted(true);
+        this.add(progressBar);
+//        progressBar.setValue(300);
+//        progressBar.setBackground(Color.white);
+//        progressBar.setForeground(Color.WHITE);
+//        progressBar.setSize(9000,2000);
+
+
+        JTextField total = new JTextField("00:00");
+        total.setFont(new Font("Arial", Font.CENTER_BASELINE, 9));
+        total.setEditable(false);
+        total.setBackground(background);
+        total.setForeground(new Color(179, 179, 179));
+        this.add(total);
+    }
+
+    public void iterate() {
+        while (num < 2000) {
+            progressBar.setValue(num);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) { }
+            num += 95;
+        }
+    }
+
+    public JProgressBar getProgressBar() {
+        return progressBar;
+    }
+}
