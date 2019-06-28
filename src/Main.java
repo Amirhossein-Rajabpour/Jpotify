0b8e74d0658e80db980804e6babe455dd8760bed
Amirmehdy Zarrinnezhad <amzarrinnezhad@gmail.com>
2019-06-28 04:48:47 +0430

    MainPage

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         WelcomePage welcomePage = new WelcomePage();
-
+//        ImageIcon zelos = new ImageIcon("/Users/apple/Desktop/JpotifyWhite.png");
+//        welcomePage.setIconImage(zelos.getImage());
     }
 }
