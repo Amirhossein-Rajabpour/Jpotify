787306732ee221c4517b78e839bebd1dacae4f58
Amirmehdy Zarrinnezhad <amzarrinnezhad@gmail.com>
2019-06-30 17:28:23 +0430

    commit for Amirhosein

--- src/controller/PlayPartController.java
+++ src/controller/PlayPartController.java
@@ -1,59 +1,40 @@
 package controller;
 
 import javazoom.jl.decoder.JavaLayerException;
-import javazoom.jl.player.advanced.AdvancedPlayer;
+import javazoom.jl.player.Player;
 
-import java.io.IOException;
 import java.io.InputStream;
 
 public class PlayPartController {
 
     private final static int NOTSTARTED = 0;
-    private final int PLAYING = 1;
-    private final int PAUSED = 2;
-    private final int FINISHED = 3;
-
-    public AdvancedPlayer getPlayer() {
-        return player;
-    }
+    private final static int PLAYING = 1;
+    private final static int PAUSED = 2;
+    private final static int FINISHED = 3;
 
     // the player actually doing all the work
-    private AdvancedPlayer player;
+    private final Player player;
+
     // locking object used to communicate with player thread
-    private Object playerLock = new Object();
+    private final Object playerLock = new Object();
+
     // status variable what player thread is doing/supposed to do
-    private int playerStatus;
-    private InputStream inputStream;
+    private int playerStatus = NOTSTARTED;
 
-    public InputStream getInputStream() {
-        return inputStream;
+    public PlayPartController(final InputStream inputStream) throws JavaLayerException {
+        this.player = new Player(inputStream);
     }
-
-    public PlayPartController(InputStream inputStream) throws JavaLayerException, IOException {
-        playerStatus = NOTSTARTED;
-        player = new AdvancedPlayer(inputStream);
-        this.inputStream = inputStream;
-    }
-
-    public int getPlayerStatus() {
-        return playerStatus;
-    }
-    //    public PauseablePlayer(final InputStream inputStream, final AudioDevice audioDevice) throws JavaLayerException {
-//        this.player = new Player(inputStream, audioDevice);
-//    }
 
     /**
      * Starts playback (resumes if paused)
      */
-    public void play(int start, int end) {
+    public void play( ) throws JavaLayerException {
         synchronized (playerLock) {
             switch (playerStatus) {
                 case NOTSTARTED:
-                    Runnable r = () -> {
-                        try {
-                            playInternal(start, end);
-                        } catch (IOException e) {
-                            e.printStackTrace();
+                    final Runnable r = new Runnable() {
+                        public void run() {
+                            playInternal();
                         }
                     };
                     final Thread t = new Thread(r);
@@ -86,6 +67,7 @@ public class PlayPartController {
     /**
      * Resumes playback. Returns true if the new state is PLAYING.
      */
+
     public boolean resume() {
         synchronized (playerLock) {
             if (playerStatus == PAUSED) {
@@ -106,16 +88,11 @@ public class PlayPartController {
         }
     }
 
-    private void playInternal(int start, int end) throws IOException {
+    private void playInternal( ) {
         while (playerStatus != FINISHED) {
             try {
-                if (start == 0) {
-                    if (!player.play(1)) {
-                        break;
-                    }
-                } else {
-                    boolean b;
-                    b = player.play(start, end);
+                if (!player.play(1)) {
+                    break;
                 }
             } catch (final JavaLayerException e) {
                 break;
@@ -149,16 +126,4 @@ public class PlayPartController {
         }
     }
 
-
-    @Override
-    public String toString() {
-        return "Player{" +
-                "PLAYING=" + PLAYING +
-                ", PAUSED=" + PAUSED +
-                ", FINISHED=" + FINISHED +
-                ", player=" + player +
-                ", playerLock=" + playerLock +
-                ", playerStatus=" + playerStatus +
-                '}';
-    }
 }
